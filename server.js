d3a3edbda59564697bda80c1eae05189090dcad2
Yeswant369 <yeswantsai9@gmail.com>
2026-02-17 21:32:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.post("/generate", async (req, res) => {
     const { prompt } = req.body;
 
     const response = await fetch(
-      "https://generativelanguage.googleapis.com/v1/models/gemini-2.0-flash:generateContent",
+      "https://generativelanguage.googleapis.com/v1/models/gemini-2.5-flash:generateContent",
       {
         method: "POST",
         headers: {
